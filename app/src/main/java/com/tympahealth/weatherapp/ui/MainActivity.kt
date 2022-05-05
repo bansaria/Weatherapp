792da2bdd25f97b637ed15f7090ba6ce1dfbda3a
Bansari <bansari.ad04@gmail.com>
2022-05-05 18:48:03 +0530

    add view binding implementaion in activity

--- app/src/main/java/com/tympahealth/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/tympahealth/weatherapp/ui/MainActivity.kt
@@ -2,13 +2,22 @@ package com.tympahealth.weatherapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.tympahealth.weatherapp.R
+import com.tympahealth.weatherapp.data.AppConstant
+import com.tympahealth.weatherapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
 }
