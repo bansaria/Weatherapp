cea9bf9619eb426508c5b5fe1893bf1d7d25a0f2
Bansari <bansari.ad04@gmail.com>
2022-05-05 17:34:52 +0530

    update JsonObject to Forecast data class for json parsing

--- app/src/main/java/com/tympahealth/weatherapp/data/api/WeatherInfoApi.kt
+++ app/src/main/java/com/tympahealth/weatherapp/data/api/WeatherInfoApi.kt
@@ -2,6 +2,7 @@ package com.tympahealth.weatherapp.data.api
 
 import com.google.gson.JsonObject
 import com.tympahealth.weatherapp.data.model.current.CurrentWeatherData
+import com.tympahealth.weatherapp.data.model.forecast.ForecastWeatherData
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -12,5 +13,5 @@ interface WeatherInfoApi {
     suspend fun getCurrentWeatherInfo(@Query("lat") lat: String, @Query("lon") lon: String, @Query("appid") appid: String): Response<CurrentWeatherData>
 
     @GET("forecast")
-    suspend fun getForecastWeatherInfo(@Query("lat") lat: String, @Query("lon") lon: String, @Query("appid") appid: String): Response<JsonObject>
+    suspend fun getForecastWeatherInfo(@Query("lat") lat: String, @Query("lon") lon: String, @Query("appid") appid: String): Response<ForecastWeatherData>
 }
