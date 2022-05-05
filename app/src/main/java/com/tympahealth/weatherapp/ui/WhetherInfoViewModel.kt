800fff93750262093ff25dfce0399fae5e1914f3
Bansari <bansari.ad04@gmail.com>
2022-05-05 16:49:19 +0530

    added WeatherInfoViewModel to get weather info from repo

---
+++ app/src/main/java/com/tympahealth/weatherapp/ui/WhetherInfoViewModel.kt
@@ -0,0 +1,15 @@
+package com.tympahealth.weatherapp.ui
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class WeatherInfoViewModel(application: Application) : AndroidViewModel(application) {
+
+    fun getCurrentWeatherInfo(latitude: String, longitude: String) {
+
+    }
+
+    fun getForecastWeatherInfo(latitude: String, longitude: String) {
+
+    }
+}
