687e24ba068617884d772fbf6c5fc0a5e3252359
Bansari <bansari.ad04@gmail.com>
2022-05-05 16:45:22 +0530

    added WeatherInfoFragment.kt

---
+++ app/src/main/java/com/tympahealth/weatherapp/ui/WeatherInfoFragment.kt
@@ -0,0 +1,19 @@
+package com.tympahealth.weatherapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tympahealth.weatherapp.R
+
+class WeatherInfoFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_weather_info, container, false)
+    }
+}
