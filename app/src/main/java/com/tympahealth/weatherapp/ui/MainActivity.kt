95f2185fb1540b7671d26f9edf9e20606322045c
Bansari <bansari.ad04@gmail.com>
2022-05-05 16:41:02 +0530

    Migrate Activity to ui package

--- app/src/main/java/com/tympahealth/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/tympahealth/weatherapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.tympahealth.weatherapp
+package com.tympahealth.weatherapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tympahealth.weatherapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
