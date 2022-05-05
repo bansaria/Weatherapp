2ff93f8ef4298d401dc3aca7c202621a4ca237ae
Bansari <bansari.ad04@gmail.com>
2022-05-05 17:11:27 +0530

    modified constructor of repo to receive api service reference

--- app/src/main/java/com/tympahealth/weatherapp/data/repository/WeatherInfoRepository.kt
+++ app/src/main/java/com/tympahealth/weatherapp/data/repository/WeatherInfoRepository.kt
@@ -1,6 +1,9 @@
 package com.tympahealth.weatherapp.data.repository
 
-class WeatherInfoRepository {
+import com.tympahealth.weatherapp.data.api.WeatherInfoApi
+import javax.inject.Inject
+
+class WeatherInfoRepository @Inject constructor(private val apiService: WeatherInfoApi) {
 
     fun getCurrentWeatherInfo(latitude: String, longitude: String) {
 
