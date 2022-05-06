99a83f0f9bc75a7ac0d5f4122fd0fc78b839464e
Bansari <bansari.ad04@gmail.com>
2022-05-06 08:35:08 +0530

    added utility to convert unix time to local time

---
+++ app/src/main/java/com/tympahealth/weatherapp/util/Util.kt
@@ -0,0 +1,13 @@
+package com.tympahealth.weatherapp.util
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+object Util {
+
+    fun convertUnixTimeLocalTime(unixTime: Int): String {
+        val timelineMilliSec = unixTime.toLong() * 1000
+        val df: Date = Date(timelineMilliSec)
+        return SimpleDateFormat("dd,MMM,yyyy hh:mma", Locale.ENGLISH).format(df)
+    }
+}
