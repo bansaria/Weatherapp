eea11fa9881a3553f811e6b818033bdb05544b11
Bansari <bansari.ad04@gmail.com>
2022-05-06 10:30:25 +0530

    add implementation in adapter to show forecast data

--- app/src/main/java/com/tympahealth/weatherapp/ui/adapter/ForecastInfoAdapter.kt
+++ app/src/main/java/com/tympahealth/weatherapp/ui/adapter/ForecastInfoAdapter.kt
@@ -1,5 +1,60 @@
 package com.tympahealth.weatherapp.ui.adapter
 
-class ForecastInfoAdapter(private val forecastList: MutableList<com.tympahealth.weatherapp.data.model.forecast.List>) {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.tympahealth.weatherapp.R
+import com.tympahealth.weatherapp.data.model.forecast.List
+import com.tympahealth.weatherapp.databinding.ItemForecastDetailsBinding
+import com.tympahealth.weatherapp.util.Util
 
+class ForecastInfoAdapter(private val context: Context, private val forecastList: MutableList<List>) : RecyclerView.Adapter<ForecastInfoAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binder = ItemForecastDetailsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binder)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(forecastList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return forecastList.size
+    }
+
+    inner class ViewHolder(private val binder: ItemForecastDetailsBinding) : RecyclerView.ViewHolder(binder.root) {
+
+        fun bind(data: List) {
+            val date = data.dt ?: 0
+            binder.tvDay.text = Util.convertUnixTimeLocalTime(date)
+
+            var humidity = 0
+
+            data.main?.let { mainData ->
+
+                mainData.temp?.let {
+                    binder.tvCurrentTemp.text = "$it"
+                }
+
+                val tempMin = mainData.tempMin ?: 0
+                val tempMax = mainData.tempMax ?: 0
+
+                binder.tvCurrentMaxMinTemp.text = "($tempMax/$tempMin) ${context.getString(R.string.text_celsius)}"
+
+                humidity = mainData.humidity ?: 0
+            }
+
+            val weather = data.weather
+            val description = if (weather.size > 0) {
+                weather[0].description
+            } else {
+                context.getString(R.string.text_no_data)
+            }
+
+            val weatherDescription = String.format(context.getString(R.string.text_weather_description), description, "$humidity%")
+            binder.tvCurrentWeatherDescription.text = weatherDescription
+        }
+    }
 }
